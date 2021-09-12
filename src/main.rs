8d26291c1dc51d5dcab386426b6402db23dc70b7
Armin AFD <armin7afd@gmail.com>
2021-09-12 12:02:03 +0430

    Implemented Unit enum and mapped each type to it's card values

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+
+enum Unit {
+    Warden,
+    Siren,
+    Keeper,
+    Saboteur,
+    Swarm,
+    Slayer,
+    Titan,
+    Ravager
+}
+
 struct Card {
-    pub name: String,
+    pub name: Unit,
     pub top: u8,
     pub right: u8,
     pub bottom: u8,
@@ -7,6 +19,19 @@ struct Card {
 }
 
 impl Card {
+    fn get_values(&self) -> [u8; 4] {
+        match self.name {
+            Unit::Warden => [6, 6, 4, 4],
+            Unit::Siren => [7, 4, 4, 5],
+            Unit::Keeper => [9, 5, 1, 5],
+            Unit::Saboteur => [6, 5, 4, 5],
+            Unit::Swarm => [7, 3, 3, 3],
+            Unit::Slayer => [1, 6, 7, 6],
+            Unit::Titan => [7, 4, 6, 3],
+            Unit::Ravager => [8, 4, 6, 2]
+        }
+    }
+    
     fn placement() {
 
     }
@@ -16,7 +41,7 @@ impl Card {
     }
 
     fn endturn() {
-        
+
     }
 }
 
