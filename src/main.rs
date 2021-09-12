70ab5467da4b9d2316ffe89223345eedb27f251f
Armin AFD <armin7afd@gmail.com>
2021-09-12 11:36:24 +0430

    Init. Card struct body

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+struct Card {
+    pub name: String,
+    pub top: u8,
+    pub right: u8,
+    pub bottom: u8,
+    pub left: u8
+}
+
+impl Card {
+    fn placement() {
+
+    }
+
+    fn attack() {
+
+    }
+
+    fn endturn() {
+        
+    }
+}
+
+
+fn main() {
+
+}
